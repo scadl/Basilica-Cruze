463be9d52c70616d9fa7d65cab70a574a3004572
Alexnadr <scad.luncher@gmail.com>
2020-05-07 15:13:22 +0300

    resources & android version update

--- BCruze_UE4/Build/Android/src/net/scadsdnd/BCruze_UE4/OBBData.java
+++ BCruze_UE4/Build/Android/src/net/scadsdnd/BCruze_UE4/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-44505263L // the length of the file in bytes
+103867490L // the length of the file in bytes
 )
 };
 };
